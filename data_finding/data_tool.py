2f5b97ec574cb20f8a5d25e02cf8460cf14f2ecf
samqyz <samqy.zheng@mail.utoronto.ca>
2025-06-05 16:13:29 -0400

    Included functions that create csv files

--- data_finding/data_tool.py
+++ data_finding/data_tool.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from habanero import Crossref
 from habanero import counts
 from habanero import cn
@@ -30,24 +29,24 @@ def find_p(laureate, field):
     return float((sorted_data[sorted_data['Is prize-winning paper'] == 'YES'].index[0] + 1)
                  / sorted_data.shape[0])
 def find_all_chem_p():
-    chem_laureates = {}
+    chem_laureates = []
     chem_unique_names = chem['Laureate name'].unique()
     for name in chem_unique_names:
-        chem_laureates.update({name: find_p(name, "chem")})
+        chem_laureates.append(find_p(name, "chem"))
     return chem_laureates
 
 def find_phys_p():
-    phys_laureates = {}
+    phys_laureates = []
     phys_unique_names = phys['Laureate name'].unique()
     for name in phys_unique_names:
-        phys_laureates.update({name: find_p(name, "phys")})
+        phys_laureates.append(find_p(name, "phys"))
     return phys_laureates
 
 def find_med_p():
-    med_laureates = {}
+    med_laureates = []
     med_unique_names = med['Laureate name'].unique()
     for name in med_unique_names:
-        med_laureates.update({name: find_p(name, "chem")})
+        med_laureates.append(find_p(name, "chem"))
     return med_laureates
 
 def find_team_size(doi):
@@ -62,7 +61,7 @@ def find_team_size(doi):
     return num_authors
 
 def find_all_team_sizes(laureate, field):
-    team_sizes = {}
+    team_sizes = []
     if field == "chem":
         df = chem
     elif field == "phys":
@@ -71,34 +70,11 @@ def find_all_team_sizes(laureate, field):
         df = med
     laureate_data = df[df['Laureate name'] == laureate]
     for row in laureate_data.itertuples():
-        team_sizes.update({row.DOI: find_team_size(row.DOI)})
+        team_sizes.append(find_team_size(row.DOI))
     return team_sizes
 
-def find_citations(doi):
-    return counts.citation_count(doi= doi)
-
-def find_all_citation_counts(laureate, field):
-    """habanero takes forever, so use this function at your own risk"""
-    paper_citations = {}
-    if field == "chem":
-        df = chem
-    elif field == "phys":
-        df = phys
-    else:
-        df = med
-    laureate_data = df[df['Laureate name'] == laureate]
-    for row in laureate_data.itertuples():
-        paper_citations.update({row.DOI: counts.citation_count(doi= row.DOI)})
-    return paper_citations
-
-def determine_gender(laureate):
-    if laureate in female_laureates:
-        return "female"
-    else:
-        return "male"
-
-def determine_laureate_genders(field):
-    laureate_genders = {}
+def find_all_teamsizes(field):
+    team_sizes = []
     if field == "chem":
         df = chem
     elif field == "phys":
@@ -107,7 +83,86 @@ def determine_laureate_genders(field):
         df = med
     laureate_names = df['Laureate name'].unique()
     for name in laureate_names:
-        laureate_genders.update({name: determine_gender(name)})
+        team_sizes += find_all_team_sizes(name, field)
+    return team_sizes
+
+def find_citations(doi):
+    return counts.citation_count(doi= doi)
+
+def find_citation_counts(laureate, field):
+    """habanero takes forever, so use this function at your own risk"""
+    paper_citations = []
+    if field == "chem":
+        df = chem
+    elif field == "phys":
+        df = phys
+    else:
+        df = med
+    laureate_data = df[df['Laureate name'] == laureate]
+    for row in laureate_data.itertuples():
+        paper_citations.append(counts.citation_count(doi= row.DOI))
+    return paper_citations
+
+def find_all_citation_counts(field):
+    citations = []
+    if field == "chem":
+        df = chem
+    elif field == "phys":
+        df = phys
+    else:
+        df = med
+    laureate_names = df['Laureate name'].unique()
+    for name in laureate_names:
+        citations += find_citation_counts(name, field)
+    return citations
+
+def determine_gender(laureate):
+    if laureate in female_laureates:
+        return "female"
+    else:
+        return "male"
+
+def determine_laureate_genders(field):
+    laureate_genders = []
+    if field == "chem":
+        df = chem
+    elif field == "phys":
+        df = phys
+    else:
+        df = med
+    laureate_names = df['Laureate name'].unique()
+    for name in laureate_names:
+        laureate_genders.append(determine_gender(name))
     return laureate_genders
+
+def create_p_and_gender_data(field):
+    if field == "chem":
+        df = chem
+        plist_to_add = find_all_chem_p()
+    elif field == "phys":
+        df = phys
+        plist_to_add = find_phys_p()
+    else:
+        df = med
+        plist_to_add = find_med_p()
+    data = {'Laureate name': list(df['Laureate name'].unique()),
+            'p-value': plist_to_add,
+            'Gender': determine_laureate_genders(field)}
+    d = pd.DataFrame(data)
+    d.to_csv('p_and_gender_data.csv', index=False)
+
+def add_citations_and_teamsize_to_csv(field):
+    if field == "chem":
+        df = chem
+    elif field == "phys":
+        df = phys
+    else:
+        df = med
+    new_data = {'citations': find_all_citation_counts(field),
+                'team sizes': find_all_teamsizes(field)}
+    d = pd.DataFrame(new_data)
+    combined_df = pd.concat([df, d], ignore_index=True)
+    combined_df.to_csv('updated' + field + 'data.csv',index=False)
+
 
 
